94e4db5d1c11b8f56f84466678f49b8ac6a06543
iwant <53087594+1want@users.noreply.github.com>
2022-02-16 14:52:51 +0800

    初始化

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,7 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <img alt="李俊yyds~~~~~" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <router-view v-slot="{ Component }">
+    <keep-alive>
+      <component :is="Component" />
+    </keep-alive>
+  </router-view>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/views/home/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <Header></Header>
+  <main-page></main-page>
+</template>
+
+<script setup>
+import Header from '@/components/header/index.vue'
+import MainPage from '@/components/main/index.vue'
+</script>
+
+<style></style>
