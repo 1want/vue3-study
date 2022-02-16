8098d1030a12e6455030315efaae2d510c9944c2
iwant <53087594+1want@users.noreply.github.com>
2022-02-16 15:00:49 +0800

    初始化布局

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,22 @@
 <template>
-  <router-view v-slot="{ Component }">
-    <keep-alive>
-      <component :is="Component" />
-    </keep-alive>
-  </router-view>
+  <div class="wrapper">
+    <router-view v-slot="{ Component }">
+      <keep-alive>
+        <component :is="Component" />
+      </keep-alive>
+    </router-view>
+  </div>
 </template>
+
+<style lang="less">
+.wrapper {
+  overflow: hidden;
+  width: 900px;
+  height: 600px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  border: 1px solid #000;
+}
+</style>
